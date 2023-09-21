a4847599d1a7bb261f27d211cdc39370e96503cd
Cyril Derc <cyril_derc@hotmail.com>
2023-09-21 12:56:13 +0200

    Exo

--- Exercices Codewars/script.js
+++ Exercices Codewars/script.js
@@ -1,23 +1,6 @@
-function howMuchILoveYou(nbPetals) {
-  let ar = [];
+/*=================Créer une fonction pour multiplier deux paramètres ======
 
-  for (let i = 0; i < nbPetals; i++) {
-    if (ar[ar.length - 1] === "I love you") {
-      ar.push("a little");
-    } else if (ar[ar.length - 1] === "a little") {
-      ar.push("a lot");
-    } else if (ar[ar.length - 1] === "a lot") {
-      ar.push("passionately");
-    } else if (ar[ar.length - 1] === "passionately") {
-      ar.push("madly");
-    } else if (ar[ar.length - 1] === "madly") {
-      ar.push("not at all");
-    } else {
-      ar.push("I love you");
-    }
-  }
-  console.log(ar[ar.length - 1]);
-  return ar[ar.length - 1];
+function multiply(a, b){
+    return (a * b)
 }
-
-howMuchILoveYou(8);
+*/
