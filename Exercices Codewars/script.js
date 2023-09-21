dcdc3a69f5adb6d3eb5d0e126999e521e7b394fe
Cyril Derc <cyril_derc@hotmail.com>
2023-09-21 14:48:20 +0200

    EXO

--- Exercices Codewars/script.js
+++ Exercices Codewars/script.js
@@ -3,4 +3,19 @@
 function multiply(a, b){
     return (a * b)
 }
+
+
+best practice :
+multiply = (a, b) => a * b;
 */
+
+// ================================= Créer une fonction pour additionner des données dans un array======================
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// function positiveSum(arr) {
+//     return arr.reduce((a,b) => a + (b > 0 ? b : 0),0)
+//   }
