69300b35da4000a8585156d5563f2db430c607e7
Cyril Derc <cyril_derc@hotmail.com>
2023-09-25 10:32:43 +0200

    EXO

--- Exercices Codewars/script.js
+++ Exercices Codewars/script.js
@@ -46,3 +46,10 @@ multiply = (a, b) => a * b;
 //     // Return a string of the number here!
 //     return String(num);
 //   }
+
+// ==================================Créer une fonction pour convertir un nombre en une String =================
+
+// ==================================Créer une fonction pour enlever la première et dernière lettre =================
+// function removeChar(str){
+//     return str.slice(1, -1)
+//     };
