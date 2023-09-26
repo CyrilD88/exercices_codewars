ebb40993a6c2cf0002e06f4931cfded4b8602352
Cyril Derc <cyril_derc@hotmail.com>
2023-09-26 17:51:10 +0200

    EXO

--- Exercices Codewars/script.js
+++ Exercices Codewars/script.js
@@ -79,3 +79,64 @@ multiply = (a, b) => a * b;
 // // arr.splice;(0, 1 ) //permet de supprimer des éléments. Le 0 c'est l'index, le 1 le nombre d'élément à supprimer. Donc ici on part du début du tableau et on supprime un élément
 // arr.splice(1, 0, -1.5); //permet d'ajouter des éléments. Le 1 c'est l'index, le 0 le nombre d'élément à supprimer. Donc ici on part deuxième élément du tableau et on rajouter -1.5
 // console.log(arr);
+
+// ==================================Méthode pour supprimer un élément à la fin d'un tableau  ===============
+
+// let arr = [1, 2, 3];
+
+// arr.pop(); //permet de supprimer le dernier élément du tableau
+
+// console.log(arr);
+
+// ==================================Méthode pour trouver un élément dans tableau  ===============
+//la méthode indexOf()
+//  la méthode indexOf ==> renvoie l'index du premier élément trouvé. Si l'élément n'est pas dans le tableau la méthode renverra -1
+// par exemple
+
+// const tableau = [1, 42, 42, 3];
+// console.log(tableau.indexOf(42));
+// console.log(tableau.indexOf(5));
+
+// la méthode indexOf permet aussi de passer en deuxième argument la position à partir de laquelle l'élément doit être recherché
+// const tableau = [1, 42, 42, 3];
+// const indexes = [];
+// let index = tableau.indexOf(42);
+// while (index !== -1) {
+//   indexes.push(index);
+//   index = tableau.indexOf(42, index + 1);
+// }
+// console.log(indexes);
+
+// La méthode includes()
+// La méthode includes() permet de vérifier si un tableau contient une valeur . Elle retourne un booléen, si le tableau contient l'élément elle retournera true, sinon false.
+// const tableau = [1, 42, 42, 3];
+// console.log(tableau.lastIndexOf(42)); // 2
+// console.log(tableau.lastIndexOf(5)); // -1
+
+// La méthode find()
+// La méthode find() renvoie le premier élément trouvé dans le tableau qui vérifie la condition testée par une fonction.
+// Cette méthode prend une fonction de rappel (callback) qui va tester la condition pour chaque élément du tableau.
+// Dès que la fonction de test retournera true, alors la méthode find() retournera l'élément. Si la condition n'est jamais vérifiée, alors elle retournera undefined.
+// const tableau = [2, 5, 8, 12, 24, 52];
+// const sup = tableau.find((el) => el > 10);
+// console.log(sup); // 12
+
+// La fonction de rappel reçoit de un à trois arguments. Vous pouvez les nommer comme vous voulez.
+
+// Le premier argument, obligatoire, est l'élément du tableau sur lequel est effectué le test. Par convention, on utilise souvent e ou el pour élément.
+// Le second argument, facultatif, est l'index de l'élément sur lequel est effectué le test. Par convention, on utilise souvent i pour index
+// Le troisième argument, facultatif est le tableau sur lequel est utilisée la méthode find(). Par convention, on utilise souvent t ou arr pour tableau ou array.
+// const tableau = [2, 5, 8, 12, 24, 52];
+// const sup = tableau.find((el, i, t) => el > 10 && i < 2);
+// console.log(sup); // undefined
+
+// Ici la fonction de test va vérifier deux choses pour chaque élément : que l'élément est supérieur à 10 et que son index est inférieur à 2.
+// Comme le test n'est jamais vérifié, la méthode retournera undefined.
+
+// La méthode findIndex()
+// la méthode findIndex() renvoie l'index du PREMIER élément trouvé dans le tableau qui vérifie la condition testée par une fonction.
+// Elle peut également utiliser les mêmes arguments que la méthode find().
+
+// const tableau = [2, 5, 8, 12, 24, 52];
+// const sup = tableau.findIndex(el => el > 10);
+// console.log(sup); // 3
